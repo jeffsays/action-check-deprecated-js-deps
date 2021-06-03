2740546a8870d552248a5205a33c40e0dee26163
Jeff Beck <jeff.beck@cumulusds.com>
2021-06-03 17:08:16 -0400

    npm audit fix; update formatting

--- index.js
+++ index.js
@@ -7,18 +7,19 @@ async function run() {
     const allDeps = getAllDeps();
     core.debug(JSON.stringify([...allDeps]));
     const deprecations = await checkForDeprecations(allDeps);
-    core.setOutput("deprecated", [...deprecations].join(","));
-    if (deprecations.size)
-      core.warning(`Deprecated: ${[...deprecations].join(", ")}`);
-    else
+    core.setOutput("deprecated", [...deprecations].join("\n"));
+    if (deprecations.size) {
+      core.warning(`Deprecated: ${[...deprecations].join("\n")}`);
+    } else {
       core.setOutput(
         "deprecated",
         "✅ Checked dependencies and no deprecated dependencies found"
       );
-    console.info(
-      "✅ Checked %d dependencies and no deprecated dependencies found",
-      allDeps.size
-    );
+      console.info(
+        "✅ Checked %d dependencies and no deprecated dependencies found",
+        allDeps.size
+      );
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
