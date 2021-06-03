87ae647f22a0d588b6a50835004180d0194b4f6d
Jeff Beck <jeff.beck@cumulusds.com>
2021-06-03 17:05:40 -0400

    set warning

--- index.js
+++ index.js
@@ -7,19 +7,18 @@ async function run() {
     const allDeps = getAllDeps();
     core.debug(JSON.stringify([...allDeps]));
     const deprecations = await checkForDeprecations(allDeps);
-    //core.setOutput("deprecated", [...deprecations].join(","));
+    core.setOutput("deprecated", [...deprecations].join(","));
     if (deprecations.size)
-      // core.setFailed(`Deprecated: ${[...deprecations].join(", ")}`);
-      core.setOutput("deprecated", [...deprecations].join(","));
+      core.warning(`Deprecated: ${[...deprecations].join(", ")}`);
     else
       core.setOutput(
         "deprecated",
         "✅ Checked dependencies and no deprecated dependencies found"
       );
-      console.info(
-        "✅ Checked %d dependencies and no deprecated dependencies found",
-        allDeps.size
-      );
+    console.info(
+      "✅ Checked %d dependencies and no deprecated dependencies found",
+      allDeps.size
+    );
   } catch (error) {
     core.setFailed(error.message);
   }
